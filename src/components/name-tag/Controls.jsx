42c57ec0b78b48001bc52989adb2445d3a7ac7f0
Peter <pete225848@gmail.com>
2021-11-05 15:35:21 -0700

    built controls component

--- src/components/name-tag/Controls.jsx
+++ src/components/name-tag/Controls.jsx
@@ -1,8 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Controls = () => ();
+const Controls = ({ greetingChange, nameChange }) => (
+  <form>
+    <label aria-label="greeting">Greeting</label>
+    <input type="text" onChange={greetingChange} />
+    <label aria-label="name">Name</label>
+    <input type="text" onChange={nameChange} />
+  </form>
+);
 
-Controls.propTypes = {};
+Controls.propTypes = {
+  greetingChange: PropTypes.string.isRequired,
+  nameChange: PropTypes.string.isRequired,
+};
 
 export default Controls;
