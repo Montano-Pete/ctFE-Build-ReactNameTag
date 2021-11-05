d7cda1046d0d996a0ef4b13c4ffd2c13446f7eb4
Peter <pete225848@gmail.com>
2021-11-05 15:36:14 -0700

    built out NameTag container

--- src/containers/NameTag.jsx
+++ src/containers/NameTag.jsx
@@ -1,5 +1,31 @@
 import React, { Component } from 'react';
+import Controls from '../components/name-tag/Controls';
+import Display from '../components/name-tag/Display';
 
 export default class NameTag extends Component {
+  state = {
+    greetingText: '',
+    nameText: '',
+  };
 
+  greetingChangeHandler = (event) => {
+    this.setState({ greetingText: event.target.value });
+  };
+
+  nameChangeHandler = (event) => {
+    this.setState({ nameText: event.target.value });
+  };
+
+  render() {
+    const { greetingText, nameText } = this.state;
+    return (
+      <>
+        <Controls
+          greetingChange={this.greetingChangeHandler}
+          nameChange={this.nameChangeHandler}
+        />
+        <Display name={nameText} greeting={greetingText} />
+      </>
+    );
+  }
 }
